5deec46c8294bd844cc11030b42bdf2cebc348cd
zheng kang <1479593039@qq.com>
2018-01-03 11:42:22 +0800

    返回jsonResult的json格式数

--- src/main/java/com/connext/ebayhelpcenter/controller/ManagerController.java
+++ src/main/java/com/connext/ebayhelpcenter/controller/ManagerController.java
@@ -33,10 +33,10 @@ public class ManagerController {
      */
     @RequestMapping(value = "/listAllMenus", method = RequestMethod.POST)
     @ResponseBody
-    public List<EbayFirstMenus> listAllTitle(HttpServletResponse response) {
+    public JsonResult listAllTitle(HttpServletResponse response) {
         response.setHeader("Access-Control-Allow-Origin", "*");//解决远端跨域访问不允许
         List<EbayFirstMenus> list = managerService.listAllTitle();
-        return list;
+        return new JsonResult(list);
     }
 
     /**
